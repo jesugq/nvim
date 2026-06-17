e2ec513316355c43af8fd00dbc606aac920f22e8
Jesús González <jesus.gonzalez@simplepractice.com>
2026-06-16 19:35:17 -0600

    Additional snack pickers

--- lua/keymaps/snacks.lua
+++ lua/keymaps/snacks.lua
@@ -7,9 +7,12 @@ do
   vim.keymap.set('n', '<leader>fc', function() Snacks.picker.command_history() end, { desc = 'Snacks picker command history' })
   vim.keymap.set('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Snacks picker files' })
   vim.keymap.set('n', '<leader>fr', function() Snacks.picker.recent() end, { desc = 'Snacks picker recent' })
+  vim.keymap.set('n', '<leader>fd', function() Snacks.picker.git_diff() end, { desc = 'Snacks picker git diff' })
   vim.keymap.set('n', '<leader>fg', function() Snacks.picker.grep() end, { desc = 'Snacks picker grep' })
   vim.keymap.set('n', '<leader>f"', function() Snacks.picker.registers() end, { desc = 'Snacks picker registers' })
+  vim.keymap.set('n', '<leader>fs', function() Snacks.picker.search_history() end, { desc = 'Snacks picker search history' })
   vim.keymap.set('n', '<leader>fh', function() Snacks.picker.help() end, { desc = 'Snacks picker help' })
+  vim.keymap.set('n', '<leader>f"', function() Snacks.picker.registers() end, { desc = 'Snacks picker registers' })
   vim.keymap.set('n', '<leader>fk', function() Snacks.picker.keymaps() end, { desc = 'Snacks picker keymaps' })
   vim.keymap.set('n', '<leader>fm', function() Snacks.picker.marks() end, { desc = 'Snacks picker marks' })
 
