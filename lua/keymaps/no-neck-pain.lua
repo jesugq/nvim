febc1adb4059a4bc9c67e687131fb38cc2291962
Jesús González <jesus.gonzalez@simplepractice.com>
2026-07-06 10:00:55 -0600

    Add widescreen monitor's no neck pain resize

--- lua/keymaps/no-neck-pain.lua
+++ lua/keymaps/no-neck-pain.lua
@@ -1,5 +1,6 @@
 do
   vim.keymap.set('n', '<leader>bm', ':NoNeckPain<CR>', { desc = 'Center buffer' })
   vim.keymap.set('n', '<leader>b9', ':NoNeckPainResize 90<CR>', { desc = 'Resize center buffer 90' })
-  vim.keymap.set('n', '<leader>b0', ':NoNeckPainResize ', { desc = 'Resize center buffer specify' })
+  vim.keymap.set('n', '<leader>b0', ':NoNeckPainResize 100<CR>', { desc = 'Resize center buffer 100' })
+  vim.keymap.set('n', '<leader>b-', ':NoNeckPainResize ', { desc = 'Resize center buffer specify' })
 end
