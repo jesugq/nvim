e8f780b9ee2a726d2af2bac4af065a84f733a7d7
Jesús González <jesus.gonzalez@simplepractice.com>
2026-07-08 17:29:34 -0600

    Remove unused keymaps after auto-lsp for ruby

--- lua/keymaps/marks.lua
+++ lua/keymaps/marks.lua
@@ -1,8 +1,3 @@
 do
   vim.keymap.set('n', 'dm', '<Plug>(Marks-delete)', { desc = 'Delete a letter mark' })
-
-  vim.keymap.set('n', 'm<leader>', '<Plug>(Marks-deletebuf)', { desc = 'Delete all marks in current buffer' })
-
-  vim.keymap.set('n', ']m', '<Plug>(Marks-prev)', { desc = 'Go to prev mark in buffer' })
-  vim.keymap.set('n', '[m', '<Plug>(Marks-next)', { desc = 'Go to next mark in buffer' })
 end
