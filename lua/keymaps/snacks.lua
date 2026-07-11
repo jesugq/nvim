e0779ba14156a5c0c9ae8be4644ec9a941c6f895
jesugq <jesugq@outlook.com>
2026-07-10 22:31:36 -0600

    Leave snacks picker lsp commands for when they work

--- lua/keymaps/snacks.lua
+++ lua/keymaps/snacks.lua
@@ -12,4 +12,14 @@ do
   vim.keymap.set('n', '<leader>sm', function() Snacks.picker.marks() end, { desc = 'Snacks picker marks' })
   vim.keymap.set('n', '<leader>sh', function() Snacks.picker.help() end, { desc = 'Snacks picker help' })
   vim.keymap.set('n', '<leader>sk', function() Snacks.picker.keymaps() end, { desc = 'Snacks picker keymaps' })
+
+  -- vim.keymap.set('n', '<leader>lf', function() Snacks.picker.lsp_definitions() end, { desc = 'Snacks picker lsp definitions' })
+  -- vim.keymap.set('n', '<leader>lc', function() Snacks.picker.lsp_declarations() end, { desc = 'Snacks picker lsp declarations' })
+  -- vim.keymap.set('n', '<leader>lr', function() Snacks.picker.lsp_references() end, { desc = 'Snacks picker lsp references' })
+  -- vim.keymap.set('n', '<leader>li', function() Snacks.picker.lsp_implementations() end, { desc = 'Snacks picker lsp implementations' })
+  -- vim.keymap.set('n', '<leader>lt', function() Snacks.picker.lsp_type_definitions() end, { desc = 'Snacks picker lsp type definitions' })
+  -- vim.keymap.set('n', '<leader>l<', function() Snacks.picker.lsp_incoming_calls() end, { desc = 'Snacks picker lsp incoming calls' })
+  -- vim.keymap.set('n', '<leader>l>', function() Snacks.picker.lsp_outgoing_calls() end, { desc = 'Snacks picker lsp outgoing calls' })
+  -- vim.keymap.set('n', '<leader>ls', function() Snacks.picker.lsp_symbols() end, { desc = 'Snacks picker lsp symbols' })
+  -- vim.keymap.set('n', '<leader>lw', function() Snacks.picker.lsp_workspace_symbols() end, { desc = 'Snacks picker lsp workspace symbols' })
 end
