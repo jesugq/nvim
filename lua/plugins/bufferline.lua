63e1b125cacc61e5274711928b048340112a4424
jesugq <jesugq@outlook.com>
2026-07-14 16:56:05 -0600

    Add config to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,4 +1,16 @@
 do
   vim.pack.add { 'https://github.com/akinsho/bufferline.nvim' }
-  require('bufferline').setup { }
+  require('bufferline').setup {
+    options = {
+      tab_size = 8,
+      max_name_length = 16,
+      color_icons = true,
+      show_buffer_icons = true,
+      show_close_icon = false,
+      separator_style = 'slant',
+      indicator = {
+        style = 'underline',
+      }
+    },
+  }
 end
