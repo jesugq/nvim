b06a799e9a1b6c1a2e1aad90d23de37727ddcf20
jesugq <jesugq@outlook.com>
2026-07-22 18:58:08 -0600

    Fix nvim-tmux-navigation

--- lua/keymaps/telescope-orgmode.lua
+++ lua/keymaps/telescope-orgmode.lua
@@ -1,6 +1,11 @@
 do
-  local tom = require('telescope-orgmode')
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'org',
+    callback = function()
+      local tom = require('telescope-orgmode')
 
-  vim.keymap.set('n', '<C-c>r', tom.refile_heading, { desc = 'Org refile' })
-  vim.keymap.set('n', '<C-c>h', tom.insert_link, { desc = 'Org insert link to headline' })
+      vim.keymap.set('n', '<C-c>r', tom.refile_heading, { desc = 'Org refile' })
+      vim.keymap.set('n', '<C-c>f', tom.insert_link, { desc = 'Org insert link to headline' })
+    end,
+  })
 end
