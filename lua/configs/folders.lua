44f4c41f4d6b6589908c2456a10c436f34e0eb71
jesugq <jesugq@outlook.com>
2026-08-12 17:39:07 -0600

    Obsidian random file improvement

--- lua/configs/folders.lua
+++ lua/configs/folders.lua
@@ -1,10 +1,7 @@
 do
   local FUNCTION = {}
 
-  FUNCTION.random_file = function(path, opts)
-    opts = opts or {}
-    local replace = opts.replace or false
-    local wrap = opts.wrap or false
+  FUNCTION.random_file = function(path)
 
     local full_path = vim.fn.expand(path)
     local files = vim.fs.find(function() return true end, {
@@ -17,22 +14,20 @@ do
       return
     end
 
-    math.randomseed(os.time())
+    math.randomseed((vim.uv or vim.loop).hrtime())
     local file = files[math.random(#files)]
 
     local prev_buffer = vim.api.nvim_get_current_buf()
+    local prev_name = vim.api.nvim_buf_get_name(prev_buffer)
     vim.cmd('edit ' .. vim.fn.fnameescape(file))
     local next_buffer = vim.api.nvim_get_current_buf()
+    local next_name = vim.api.nvim_buf_get_name(next_buffer)
 
     local prev_valid = vim.api.nvim_buf_is_valid(prev_buffer)
     local prev_diff = prev_buffer ~= next_buffer
+    local prev_sibling = vim.fs.dirname(prev_name) == vim.fs.dirname(next_name)
 
-    if wrap then
-      vim.wo.wrap = true
-      vim.wo.linebreak = true
-    end
-
-    if replace and prev_valid and prev_diff then
+    if prev_valid and prev_diff and prev_sibling then
       pcall(Snacks.bufdelete.delete, { buf = prev_buffer })
     end
   end
