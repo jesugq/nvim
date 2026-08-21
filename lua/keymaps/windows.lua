796c26291de934d3996ffd6ba734628f9e6b84b3
jesugq <jesugq@outlook.com>
2026-08-20 19:07:33 -0600

    Use window swap not window move

--- lua/keymaps/windows.lua
+++ lua/keymaps/windows.lua
@@ -1,4 +1,18 @@
 do
+  local function swap_window(direction)
+    local source_win = vim.api.nvim_get_current_win()
+    vim.cmd('wincmd ' .. direction)
+    local target_win = vim.api.nvim_get_current_win()
+
+    if source_win ~= target_win then
+      local source_buf = vim.api.nvim_win_get_buf(source_win)
+      local target_buf = vim.api.nvim_win_get_buf(target_win)
+
+      vim.api.nvim_win_set_buf(source_win, target_buf)
+      vim.api.nvim_win_set_buf(target_win, source_buf)
+    end
+  end
+
   vim.keymap.set('n', '<C-w>c', '<Nop>', { desc = 'Close current window' })
   vim.keymap.set('n', '<C-w>x', '<C-w>c', { remap = false, desc = 'Close current window' })
 
@@ -6,8 +20,8 @@ do
   vim.keymap.set('n', '<C-w>J', '<Nop>', { desc = 'Move window down' })
   vim.keymap.set('n', '<C-w>K', '<Nop>', { desc = 'Move window up' })
   vim.keymap.set('n', '<C-w>L', '<Nop>', { desc = 'Move window right' })
-  vim.keymap.set('n', '<C-w>h', '<C-w>H', { remap = false, desc = 'Move window left' })
-  vim.keymap.set('n', '<C-w>j', '<C-w>J', { remap = false, desc = 'Move window down' })
-  vim.keymap.set('n', '<C-w>k', '<C-w>K', { remap = false, desc = 'Move window up' })
-  vim.keymap.set('n', '<C-w>l', '<C-w>L', { remap = false, desc = 'Move window right' })
+  vim.keymap.set('n', '<C-w>h', function() swap_window('h') end, { desc = 'Swap window left' })
+  vim.keymap.set('n', '<C-w>j', function() swap_window('j') end, { desc = 'Swap window down' })
+  vim.keymap.set('n', '<C-w>k', function() swap_window('k') end, { desc = 'Swap window up' })
+  vim.keymap.set('n', '<C-w>l', function() swap_window('l') end, { desc = 'Swap window right' })
 end
