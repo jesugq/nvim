d3f5cadf9da13ca416b364849e89ae3ece48a6ac
jesugq <jesugq@outlook.com>
2026-08-20 18:53:38 -0600

    Fix outer tag matching

--- lua/tags.lua
+++ lua/tags.lua
@@ -16,7 +16,7 @@ do
     },
   }
   og.outer = {
-    description = 'Inner',
+    description = 'Outer',
     types = {
       {
         type = 'tags',
@@ -32,17 +32,17 @@ do
     md.inner = '<cmd>Obsidian tags macos<CR><cmd>'
     md.outer = '<cmd>Obsidian tags prime mated fusn poke nova<CR><cmd>'
     og.inner.types[1].match = 'macos'
-    og.outer.types[1].match = 'prime+mated+fusn+poke+nova'
+    og.outer.types[1].match = 'prime|mated|fusn|poke|nova'
   elseif host_name == 'prime' then
     md.inner = '<cmd>Obsidian tags prime<CR><cmd>'
     md.outer = '<cmd>Obsidian tags mated macos fusn poke nova<CR><cmd>'
     og.inner.types[1].match = 'prime'
-    og.outer.types[1].match = 'mated+macos+fusn+poke+nova'
+    og.outer.types[1].match = 'mated|macos|fusn|poke|nova'
   else
     md.inner = '<cmd>Obsidian tags mated<CR><cmd>'
     md.outer = '<cmd>Obsidian tags prime macos fusn poke nova<CR><cmd>'
     og.inner.types[1].match = 'mated'
-    og.outer.types[1].match = 'prime+macos+fusn+poke+nova'
+    og.outer.types[1].match = 'prime|macos|fusn|poke|nova'
   end
 
   FUNCTION.md = md
