943e357bcb0e0ea4921354979bb2dd6285575c07
Jesús González <jesus.gonzalez@simplepractice.com>
2026-08-28 12:09:21 -0600

    Forgot to apply DONE to orgmode autocfg

--- lua/configs/orgmode.lua
+++ lua/configs/orgmode.lua
@@ -2,11 +2,11 @@ do
   local FUNCTION = {}
 
   FUNCTION.highlights = function()
-    vim.api.nvim_set_hl(0, 'OrgRedoLine', { fg = '#768390' })
-    vim.api.nvim_set_hl(0, "OrgRedoWord", { fg = "#4d9391", bold = true })
-    vim.fn.matchadd("OrgRedoLine", [[\v^\*+\s+\zsREDO.*$]])
-    vim.fn.matchadd("OrgRedoLine", [[\v(^|\n)\*+\s+REDO\_s*.*(\n\zs[^*].*)*]])
-    vim.fn.matchadd("OrgRedoWord", [[\v^\*+\s+\zsREDO\ze\s+]])
+    vim.api.nvim_set_hl(0, 'OrgDoneLine', { fg = '#768390' })
+    vim.api.nvim_set_hl(0, "OrgDoneWord", { fg = "#4d9391", bold = true })
+    vim.fn.matchadd("OrgDoneLine", [[\v^\*+\s+\zsDONE.*$]])
+    vim.fn.matchadd("OrgDoneLine", [[\v(^|\n)\*+\s+DONE\_s*.*(\n\zs[^*].*)*]])
+    vim.fn.matchadd("OrgDoneWord", [[\v^\*+\s+\zsDONE\ze\s+]])
 
     local priority_groups = {
       "@org.priority.highest",
