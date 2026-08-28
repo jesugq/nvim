635e962456a462f060d9aa4f92fadc01c2b95c04
Jesús González <jesus.gonzalez@simplepractice.com>
2026-08-28 12:16:26 -0600

    Deprecate the use of <leader>f

--- lua/keymaps/buffers.lua
+++ lua/keymaps/buffers.lua
@@ -2,8 +2,8 @@ do
   vim.keymap.set('n', '<leader>qq', ':qa<CR>', { desc = 'Quit all' })
   vim.keymap.set('n', '<leader>qr', ':restart<CR>', { desc = 'Quit restart'})
 
-  vim.keymap.set('n', '<leader>fs', ':w<CR>', { desc = 'File save' })
-  vim.keymap.set('n', '<leader>fS', ':wa<CR>', { desc = 'File save all' })
+  vim.keymap.set('n', '<leader>bs', ':w<CR>', { desc = 'File save' })
+  vim.keymap.set('n', '<leader>bS', ':wa<CR>', { desc = 'File save all' })
 
   vim.keymap.set('n', '<leader><leader>', '<C-^>', { desc = 'Buffer switch' })
 
