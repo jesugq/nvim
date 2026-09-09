cb04d9da31757badd977ef9fcd77ddd6a90b093e
Jesús González <jesus.gonzalez@simplepractice.com>
2026-09-09 16:30:15 -0600

    Missing auto session command

--- lua/keymaps/auto-session.lua
+++ lua/keymaps/auto-session.lua
@@ -1,6 +1,7 @@
 do
   vim.keymap.set('n', '<leader>qw', function() vim.cmd('AutoSession toggle') end, { desc = 'Session save toggle' })
   vim.keymap.set('n', '<leader>qs', function() vim.cmd('AutoSession save') end, { desc = 'Session save' })
+  vim.keymap.set('n', '<leader>qc', ':AutoSession save ', { desc = 'Session create' })
   vim.keymap.set('n', '<leader>qf', function() vim.cmd('AutoSession search') end, { desc = 'Session find from' })
   vim.keymap.set('n', '<leader>qd', function() vim.cmd('AutoSession deletePicker') end, { desc = 'Session delete from' })
 end
