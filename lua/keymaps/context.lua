0a76e9b40a08cf2851e02169ce2a35ba8919a1ec
jesugq <jesugq@outlook.com>
2026-09-19 18:11:11 -0600

    Change yank to copy since it's for copying info rather than yanking files

--- lua/keymaps/context.lua
+++ lua/keymaps/context.lua
@@ -1,36 +1,36 @@
 do
-  local function yank_notify_path(message)
-    vim.notify(message, vim.log.levels.INFO, { id = 'yank_path', title = 'Yank Path', })
+  local function copy_notify_path(message)
+    vim.notify(message, vim.log.levels.INFO, { id = 'copy_path', title = 'copy Path', })
   end
 
-  local function yank_working_path()
-    local info = vim.fn.getcwd()
+  local function copy_absolute()
+    local info = vim.fn.expand('%:p')
 
     vim.fn.setreg('+', info)
     vim.fn.setreg('"', info)
 
-    yank_notify_path('Yanked working path: ' .. info)
+    copy_notify_path('Copied absolute: ' .. info)
   end
 
-  local function yank_relative_path()
-    local info = vim.fn.expand('%:.')
+  local function copy_absolute_url()
+    local info = vim.uri_encode(vim.fn.expand('%:p'))
 
     vim.fn.setreg('+', info)
     vim.fn.setreg('"', info)
 
-    yank_notify_path('Yanked relative file: ' .. info)
+    copy_notify_path('Copied absolute url: ' .. info)
   end
 
-  local function yank_encoded_path()
-    local info = vim.uri_encode(vim.fn.expand('%:.'))
+  local function copy_file()
+    local info = vim.fn.expand('%:t')
 
     vim.fn.setreg('+', info)
     vim.fn.setreg('"', info)
 
-    yank_notify_path('Yanked encoded file: ' .. info)
+    copy_notify_path('Copied file: ' .. info)
   end
 
-  local function yank_relative_path_line_n()
+  local function copy_line()
     local path = vim.fn.expand('%:.')
     local line = vim.api.nvim_win_get_cursor(0)[1]
 
@@ -39,10 +39,10 @@ do
     vim.fn.setreg('+', info)
     vim.fn.setreg('"', info)
 
-    yank_notify_path('Yanked relative file line: ' .. info)
+    copy_notify_path('Copied line: ' .. info)
   end
 
-  local function yank_relative_path_line_x()
+  local function copy_lixe()
     local path = vim.fn.expand('%:.')
 
     local start_line = vim.fn.line('v')
@@ -61,15 +61,45 @@ do
 
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), 'n', true)
 
-    yank_notify_path('Yanked relative file line: ' .. path)
+    copy_notify_path('Copied lixe: ' .. path)
   end
 
-  -- <leader>y
-  vim.keymap.set('n', '<leader>ye', yank_encoded_path, { desc = 'Yank encoded path' })
-  vim.keymap.set('n', '<leader>yf', yank_relative_path, { desc = 'Yank relative path' })
-  vim.keymap.set('n', '<leader>yl', yank_relative_path_line_n, { desc = 'Yank relative path line' })
-  vim.keymap.set('x', '<leader>yl', yank_relative_path_line_x, { desc = 'Yank relative path line' })
-  vim.keymap.set('n', '<leader>yw', yank_working_path, { desc = 'Yank working path' })
-  vim.keymap.set('n', '<leader>yd', 'ggVGD', { desc = 'Yank kill' })
-  vim.keymap.set('n', '<leader>yy', 'ggVGY', { desc = 'Yank copy' })
+  local function copy_relative()
+    local info = vim.fn.expand('%:.')
+
+    vim.fn.setreg('+', info)
+    vim.fn.setreg('"', info)
+
+    copy_notify_path('Copied relative: ' .. info)
+  end
+
+  local function copy_relative_url()
+    local info = vim.uri_encode(vim.fn.expand('%:.'))
+
+    vim.fn.setreg('+', info)
+    vim.fn.setreg('"', info)
+
+    copy_notify_path('Copied relative url: ' .. info)
+  end
+
+  local function copy_working()
+    local info = vim.fn.getcwd()
+
+    vim.fn.setreg('+', info)
+    vim.fn.setreg('"', info)
+
+    copy_notify_path('Copied working: ' .. info)
+  end
+
+  -- <leader>c
+  vim.keymap.set('n', '<leader>ca', copy_absolute, { desc = 'Copy absolute' })
+  vim.keymap.set('n', '<leader>cA', copy_absolute_url, { desc = 'Copy absolute url' })
+  vim.keymap.set('n', '<leader>cd', 'ggVGD', { desc = 'Copy dd' })
+  vim.keymap.set('n', '<leader>cf', copy_file, { desc = 'Copy file' })
+  vim.keymap.set('n', '<leader>cl', copy_line, { desc = 'Copy line' })
+  vim.keymap.set('x', '<leader>cl', copy_lixe, { desc = 'Copy lixe' })
+  vim.keymap.set('n', '<leader>cr', copy_relative, { desc = 'Copy relative' })
+  vim.keymap.set('n', '<leader>cR', copy_relative_url, { desc = 'Copy relative url' })
+  vim.keymap.set('n', '<leader>cw', copy_working, { desc = 'Copy working' })
+  vim.keymap.set('n', '<leader>cy', 'ggVGY', { desc = 'Copy yy' })
 end
